2fc0bb804f9014a38a88ab3a31770255176aede6
aBushara <aisha.bushara@gmail.com>
2023-11-29 19:36:45 +0100

    Added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>Aisha Bushara</p>
+            <p>2023</p>
+        </footer>
+    );
+};
+
+export default Footer;
